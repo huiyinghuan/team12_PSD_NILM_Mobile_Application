604a85affaa503946354aed99bad974a91d6e697
huiyinghuan <huanhuiying.hhy@gmail.com>
2024-03-01 15:18:58 +0800

    added a button for edit device for the list of devices

--- lib/pages/devices/listDevice.dart
+++ lib/pages/devices/listDevice.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:l3homeation/components/custom_button.dart';
 import 'package:l3homeation/models/iot_device.dart';
 import 'package:l3homeation/widget/navigation_drawer_widget.dart';
 import 'package:l3homeation/services/userPreferences.dart';
+import 'package:l3homeation/pages/editDevice/edit_light.dart';
 
 class ListDevice extends StatefulWidget {
   @override
@@ -171,6 +173,16 @@ class _ListDeviceState extends State<ListDevice> {
                       swapper(device);
                     },
                   ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EditLight()),
+                    );
+                  },
+                  child: Text('Edit Device'),
                 ),
               ],
             ),
